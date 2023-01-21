153c8e80ef193f2da8449883dd49870ae48b4216
Pavel <pavel_chermyanin@mail.ru>
2023-01-21 21:39:56 +0500

    described the schemas and filled them by data,connected to the sanity, implemented the functions fetching for categories and products,laid out the home page

--- apple-sanity-cms/schemas/index.ts
+++ apple-sanity-cms/schemas/index.ts
@@ -1,1 +1,4 @@
-export const schemaTypes = []
+import categories from "./categories";
+import products from "./products";
+
+export const schemaTypes = [products,categories]
--- components/Landing.tsx
+++ components/Landing.tsx
@@ -1,14 +1,37 @@
+import Image from "next/image";
 import React from "react";
+import Button from "./Button";
 
 const Landing = () => {
   return (
-    <section>
-      <div>
-        <h1>
-          <span className="primaryGradient block ">Powered</span>
+    <section className="sticky top-0 mx-auto flex h-screen max-w-[1350px] items-center justify-between px-8">
+      <div className="space-y-8">
+        <h1 className="space-y-3 text-5xl font-semibold tracking-wide lg:text-6xl xl:text-7xl">
+          <span className="primaryGradient block bg-clip-text text-transparent">
+            Powered
+          </span>
           <span className="block">By Intellect</span>
           <span className="block">Driven By Values</span>
         </h1>
+
+        <div className="space-x-8">
+          <Button title="Buy Now" />
+          <a
+            href="#"
+            className="link"
+          >
+            Learn More
+          </a>
+        </div>
+      </div>
+
+      <div className="relative hidden h-[450px] w-[450px] transition-all duration-500 md:inline lg:h-[600px] lg:w-[600px]">
+        <Image
+          src="/iphone.png"
+          layout="fill"
+          objectFit="contain"
+          alt="mainImg"
+        />
       </div>
     </section>
   );
